bde8510f983d0833007e8e60d0cb1da2ff301768
jinli626 <19742464932@163.com>
2026-06-24 00:01:25 +0800

    全面升级优化

---
+++ freshmarket/cart/context_processors.py
@@ -0,0 +1,14 @@
+from django_redis import get_redis_connection
+
+
+def cart_count(request):
+    count = 0
+    user = getattr(request, 'user', None)
+
+    if user and user.is_authenticated:
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        for value in conn.hvals(cart_key):
+            count += int(value)
+
+    return {'cart_count': count}
